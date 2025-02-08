33fc7ac047897995eb8e26fc5737b6a037bd983b
FaizanAtif <FAIZANATIF183@GMAIL.COM>
2025-02-08 06:50:05 +0500

    33

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,20 +1,18 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-18'
 
-export const dataset = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_DATASET,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
-)
+// Agar environment variable defined nahin hai to 'production' default value use hogi.
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
 
 export const projectId = assertValue(
   process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+  'hcaqqj2i'
 )
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
     throw new Error(errorMessage)
   }
-
   return v
 }
